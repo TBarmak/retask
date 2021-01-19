ba236255283c8869c7abbfa6a8025557ac60dce3
Taylor Barmak <taylorbrmk@gmail.com>
2021-01-19 16:50:18 -0500

    Stylize the NoToDos screen according to the theme
    The NoToDos screen is now stylized according to the color scheme in shared/constants.dart.
    
    The FloatingActionButton has been moved from the Scaffold to be a Positioned widget in a stack so that the NoToDos widget can circle its location.

--- lib/screens/home/no_to_dos.dart
+++ lib/screens/home/no_to_dos.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:retask/shared/constants.dart';
 import 'package:retask/shared/shared.dart';
+import 'dart:math';
 
 class NoToDos extends StatelessWidget {
   @override
@@ -11,24 +13,35 @@ class NoToDos extends StatelessWidget {
         builder: (BuildContext context, BoxConstraints constraints) {
       return Stack(
         children: [
-          Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text("You have nothing to do :)",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.grey[500], fontSize: 70)),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text("Click here to create a new to-do",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.grey[500], fontSize: 30)),
-              )
-            ],
-          )),
+          Positioned(
+            left: screenWidth(context) * 0.05,
+            child: SizedBox(
+              width: screenWidth(context) * 0.7,
+              child: Text("You have nothing to do :)",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(color: accentColor1, fontSize: 70, shadows: [
+                    Shadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 25,
+                        color: Colors.black38)
+                  ])),
+            ),
+          ),
+          Positioned(
+            top: screenHeight(context) * 0.5,
+            left: screenWidth(context) * 0.6,
+            child: SizedBox(
+              width: screenWidth(context) * 0.4,
+              child: Text("Create a new to-do",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: accentColor1, fontSize: 30, shadows: [
+                    Shadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 25,
+                        color: Colors.black38)
+                  ])),
+            ),
+          ),
           CustomPaint(
               painter: MyPainter(fullWidth, fullHeight),
               size: Size(constraints.maxWidth, constraints.maxHeight)),
@@ -46,21 +59,25 @@ class MyPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    double radius = screenWidth * 0.09;
     Paint linePaint = Paint();
+
     linePaint.strokeWidth = 7;
-    linePaint.color = Colors.grey[500];
+    linePaint.color = accentColor2;
     linePaint.style = PaintingStyle.stroke;
     linePaint.strokeCap = StrokeCap.round;
-    canvas.drawLine(Offset(screenWidth * 0.5, screenHeight * 0.71),
-        Offset(screenWidth * 0.75, screenHeight * 0.77), linePaint);
-    canvas.drawCircle(Offset(screenWidth * 0.893, screenHeight * 0.819),
-        screenWidth * 0.09, linePaint);
-
-    var path = Path();
-    path.moveTo(screenWidth * 0.65, screenHeight * 0.78);
-    path.lineTo(screenWidth * 0.75, screenHeight * 0.77);
-    path.lineTo(screenWidth * 0.7, screenHeight * 0.72);
-    canvas.drawPath(path, linePaint);
+    canvas.drawLine(
+        Offset(screenWidth * 0.8, screenHeight * 0.62),
+        // Get a point on the circle
+        Offset(
+            screenWidth * 0.87,
+            -1 *
+                    sqrt(pow(radius, 2) -
+                        pow(screenWidth * 0.87 - screenWidth * 0.893, 2)) +
+                screenHeight * 0.819),
+        linePaint);
+    canvas.drawCircle(
+        Offset(screenWidth * 0.893, screenHeight * 0.819), radius, linePaint);
   }
 
   @override
--- lib/screens/home/to_dos.dart
+++ lib/screens/home/to_dos.dart
@@ -5,6 +5,8 @@ import 'package:retask/screens/home/to_do_list.dart';
 import 'package:retask/services/to_do_service.dart';
 
 import 'package:provider/provider.dart';
+import 'package:retask/shared/constants.dart';
+import 'package:retask/shared/shared.dart';
 
 class ToDos extends StatefulWidget {
   @override
@@ -20,19 +22,38 @@ class _ToDosState extends State<ToDos> {
     return StreamProvider<List<ToDo>>.value(
       value: toDoService.toDos,
       child: Scaffold(
-          appBar: AppBar(backgroundColor: Colors.blue, title: Text("To-Do's")),
-          body: ToDoList(),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                dynamic result =
-                    await Navigator.pushNamed(context, '/new_to_do');
-                if (result != null) {
-                  setState(() {
-                    toDoService.addToDo(result['toDo']);
-                  });
-                }
-              },
-              child: Icon(Icons.add))),
+        backgroundColor: backgroundColor,
+        appBar: AppBar(backgroundColor: backgroundColor, elevation: 0.0),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/check.png"),
+                      fit: BoxFit.fitWidth)),
+            ),
+            ToDoList(),
+            Positioned(
+              width: screenWidth(context) * 0.14,
+              height: screenWidth(context) * 0.14,
+              left: screenWidth(context) * 0.893 - screenWidth(context) * 0.07,
+              top: screenHeight(context) * 0.819 - screenWidth(context) * 0.07,
+              child: FloatingActionButton(
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/new_to_do');
+                    if (result != null) {
+                      setState(() {
+                        toDoService.addToDo(result['toDo']);
+                      });
+                    }
+                  },
+                  backgroundColor: accentColor1,
+                  child: Icon(Icons.add, color: backgroundColor)),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
